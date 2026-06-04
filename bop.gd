34495fc09ee72613061beb2ca6dc0642ee2df57d
Ks-Marbs <nykolask11@gmail.com>
2026-06-03 23:29:09 -0300

    a

--- bop.gd
+++ bop.gd
@@ -11,8 +11,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	match str(self.name)[0]:
+		"0":
+			size = Vector2((get_window().size[0]+120)/4+28,60)
+		"1":
+			size = Vector2((get_window().size[0]+120)/4,60)
+			position = Vector2((get_window().size[0]+120)/4-28,0)
+		"2":
+			size = Vector2((get_window().size[0]+120)/4,60)
+			position = Vector2((get_window().size[0]+120)/2-56,0)
+		"3":
+			size = Vector2((get_window().size[0]+120)/4,60)
+			position = Vector2((get_window().size[0]+120)/4*3-84,0)
+	$RichTextLabel.position = Vector2(size[0]/4,5000/(get_window().size[0]^2))
+	$RichTextLabel.scale = Vector2((get_window().size[0]+120.0)/288.0,(get_window().size[0]+120.0)/288.0)
 	pass
-
-
-func _on_series_mouse_entered() -> void:
-	pass # Replace with function body.
