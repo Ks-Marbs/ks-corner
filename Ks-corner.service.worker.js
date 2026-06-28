2507000a6e795e8d13114d170a1bebe2f3b4611f
Ks-Marbs <nykolask11@gmail.com>
2026-06-28 16:39:57 -0300

    test 11 exported

--- Ks-corner.service.worker.js
+++ Ks-corner.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782612007|668609180';
+const CACHE_VERSION = '1782675570|1123473515';
 /** @type {string} */
 const CACHE_PREFIX = 'Ks-corner-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
